55d743b03e0397f93aa0cf4297423eb371384e85
unknown <davidalejandro64@gmail.com>
2020-10-10 02:18:52 -0500

    create errors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 
 // Controllers
@@ -21,8 +23,20 @@ app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false}));
+app.use(session({ cookie: { maxAge: 30 }, 
+    secret: 'woot',
+    resave: false, 
+    saveUninitialized: false}));
+app.use(flash());
 
+// Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 // Routes
 app.use('/', homeRouter)
--- home/services.js
+++ home/services.js
@@ -1,43 +1,72 @@
 function home(req, res) {
-  res.render('index')
+  res.render("index");
 }
 
 function calcularValor(req, res) {
-  const { body: data} = req;
+  const { body: data } = req;
   const pregunta = data.pregunta;
   const descuento = parseFloat(data.descuento);
   const valor = parseFloat(data.valor);
-  let valorPagar = valor;
-  let resta = 0;
-  let valorDescuento = 0;
-  let respuesta;  
+  let valorPagar = valor,
+    resta = 0,
+    valorDescuento,
+    respuesta;
+  const errors = [];
 
-  if(valor < 48029000) {
-    valorPagar *= 0.015
-  } else if(valor > 48029000 && valor < 108063000){
-    valorPagar *= 0.025
+  if (pregunta == undefined) {
+    errors.push({ text: "Debes de escoger una opcion: Si o No" });
+  }
+  if (data.descuento == "" && data.pregunta == 1) {
+    errors.push({ text: "Debes de escribir el descuento" });
+  }
+  if (data.descuento > 100 || data.descuento < 0) {
+    errors.push({ text: "El valor ingresado esta fuera de los limites" });
+  }
+  if (data.valor === "") {
+    errors.push({ text: "Escribe el avaluo comercial" });
+  }
+  if (errors.length > 0) {
+    res.render("index", {
+      errors,
+      descuento: data.descuento,
+      valor: data.valor,
+    });
   } else {
-    valorPagar *= 0.035
+    if (valor < 48029000) {
+      valorPagar *= 0.015;
+    } else if (valor > 48029000 && valor < 108063000) {
+      valorPagar *= 0.025;
+    } else {
+      valorPagar *= 0.035;
+    }
+    if (pregunta == 1) {
+      resta = valorPagar * (descuento / 100);
+      valorDescuento = valorPagar - resta;
+      respuesta = true;
+    }
+    const formatterPeso = new Intl.NumberFormat("es-CO", {
+      style: "currency",
+      currency: "COP",
+      minimumFractionDigits: 0,
+    });
+    const valorF = formatterPeso.format(valor).replace("COP", "$");
+    const valorPagarF = formatterPeso.format(valorPagar).replace("COP", "$");
+    const valorDescuentoF = formatterPeso
+      .format(valorDescuento)
+      .replace("COP", "$");
+    const restaF = formatterPeso.format(resta).replace("COP", "$");
+    res.render("liquidacion", {
+      valorPagarF,
+      valorPagar,
+      valorDescuentoF,
+      respuesta,
+      restaF,
+      valorF,
+    });
   }
-
-  if (pregunta == 1) {
-    resta = (valorPagar * (descuento / 100));
-    valorDescuento = valorPagar - resta
-    respuesta = true;
-  }
-  const formatterPeso = new Intl.NumberFormat('es-CO', {
-    style: 'currency',
-    currency: 'COP',
-    minimumFractionDigits: 0
-  })
-  const valorF = formatterPeso.format(valor).replace('COP', '$');
-  const valorPagarF = formatterPeso.format(valorPagar).replace('COP', '$');
-  const valorDescuentoF = formatterPeso.format(valorDescuento).replace('COP', '$');
-  const restaF = formatterPeso.format(resta).replace('COP', '$');
-  res.render('liquidacion', { valorPagarF, valorPagar, valorDescuentoF, respuesta, restaF, valorF } )
 }
 
 module.exports = {
   home,
-  calcularValor
-}
+  calcularValor,
+};
